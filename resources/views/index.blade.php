a3d6ba66383cedd40f915f1dc3cc072a25f812a4
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2022-06-03 09:42:31 +0530

    Completed index.blade.php and index function in TaskController.php.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,30 +12,35 @@
         <div class="clearfix"></div>
     </div>
 
-    <div class="card">
-        <div class="card-header">
-            First Task
-            <span class="badge text-bg-warning">Time</span>
-        </div>
-        <div class="card-body">
-            <div class="card-text">
-                <div class="float-start">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae dignissimos eius iusto
-                    laborum
-                    maxime officia saepe tempora tempore. A architecto commodi consequatur, deleniti libero omnis?
-                    Assumenda
-                    illo provident temporibus.
-                    <br>
-                    <span class="badge text-bg-info">Todo</span>
-                    <small>Last Updated - </small>
-                </div>
-                <div class="float-end">
-                    <a href="{{ route('task.edit', 1) }}" class="btn btn-success">Edit</a>
-                    <a href="{{ route('task.edit', 1) }}" class="btn btn-danger">Delete</a>
-                </div>
-                <div class="clearfix"></div>
+    @foreach($tasks as $task)
+        <div class="card">
+            <div class="card-header">
+                {{ $task->title }}
+                <span class="badge text-bg-warning">{{ $task->created_at->diffForHumans() }}</span>
+            </div>
+            <div class="card-body">
+                <div class="card-text">
+                    <div class="float-start">
+                        {{ $task->description }}
+                        <br>
 
+                        @if($task->status == 'Todo')
+                            <span class="badge text-bg-info text-dark">Todo</span>
+                        @else
+                            <span class="badge text-bg-success text-white">Done</span>
+                        @endif
+
+                        <small>Last Updated - {{ $task->updated_at->diffForHumans() }}</small>
+                    </div>
+                    <div class="float-end">
+                        <a href="{{ route('task.edit', $task->id) }}" class="btn btn-success">Edit</a>
+                        <a href="{{ route('task.edit', $task->id) }}" class="btn btn-danger">Delete</a>
+                    </div>
+                    <div class="clearfix"></div>
+
+                </div>
             </div>
         </div>
-    </div>
+    @endforeach
+
 @endsection

79a339bf642016e99dd10c1d83f94282395564f1
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2022-06-03 08:53:32 +0530

    Changes to index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,39 @@
 
 @section('main-content')
 
-    <h4 class="pb-3">My Tasks</h4>
+    <div>
+        <div class="float-start">
+            <h4 class="pb-3">My Tasks</h4>
+        </div>
+        <div class="float-end">
+            <a href="{{ route('task.create') }}" class="btn btn-info">Create Task</a>
+        </div>
+        <div class="clearfix"></div>
+    </div>
 
     <div class="card">
         <div class="card-header">
             First Task
+            <span class="badge text-bg-warning">Time</span>
         </div>
         <div class="card-body">
             <div class="card-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae dignissimos eius iusto laborum
-                maxime officia saepe tempora tempore. A architecto commodi consequatur, deleniti libero omnis? Assumenda
-                illo provident temporibus.
+                <div class="float-start">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam beatae dignissimos eius iusto
+                    laborum
+                    maxime officia saepe tempora tempore. A architecto commodi consequatur, deleniti libero omnis?
+                    Assumenda
+                    illo provident temporibus.
+                    <br>
+                    <span class="badge text-bg-info">Todo</span>
+                    <small>Last Updated - </small>
+                </div>
+                <div class="float-end">
+                    <a href="{{ route('task.edit', 1) }}" class="btn btn-success">Edit</a>
+                    <a href="{{ route('task.edit', 1) }}" class="btn btn-danger">Delete</a>
+                </div>
+                <div class="clearfix"></div>
+
             </div>
         </div>
     </div>
